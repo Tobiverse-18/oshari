04e64d5ba9b1789121a80553416767bb1bb7f69f
Tobiverse-18 <allibaloguntomiwa@gmail.com>
2026-07-25 18:05:11 +0100

    Added dynamic products and deployment configuration

--- oshari_app/views.py
+++ oshari_app/views.py
@@ -2,7 +2,14 @@ from django.shortcuts import render
 from .models import Product
 
 
-from django.http import HttpResponse
-
 def home(request):
-    return render(request, "home/home.html")
+    new_drops = Product.objects.filter(
+        is_available=True,
+        is_new_drop=True
+    )[:8]
+
+    context = {
+        "new_drops": new_drops,
+    }
+
+    return render(request, "home/home.html", context)
